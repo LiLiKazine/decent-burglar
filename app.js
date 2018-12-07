f370ca1c830a9d22825150bdbffbc7f78fda3198
Sheng Li <l.sheng@haihangyun.com>
2018-12-07 17:18:51 +0800

    basic function

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const load = require('./artinstitutechi/ai_chi')
+const ai_chi = require('./artinstitutechi/ai_chi')
+const request = require("request")
+const Agent = require('socks5-https-client/lib/Agent')
+const fs = require('fs')
 
-load(3)
+let page = 1
+
+let chi_cb = (data_arr) => {
+    data_arr.forEach(ele => {
+        let url = `${ele.iiifid}/full/!4000,4000/0/default.jpg`
+        let title = ele.title.replace(/[\\\/:*?"<>|]+/g, "_")
+        request({
+            url: url,
+            agentClass: Agent
+        }).pipe(fs.createWriteStream(`${title}.jpg`))
+    })
+    if (data_arr.length > 0) {
+        chi(page+1)
+    }
+}
+
+let chi = (page) => {
+
+    ai_chi(page, chi_cb)
+
+}
+
+chi()
--- artinstitutechi/ai_chi.js
+++ artinstitutechi/ai_chi.js
@@ -6,31 +6,22 @@ const base_url = 'https://www.artic.edu/collection'
 
 let laod_more = (page, callback) => {
 
+    let tag_base = 'li.m-listing a'  
+    let tag_img = 'span.m-listing__img img' 
+    let tag_title = 'span.m-listing__meta strong.title'
+    let tag_author = 'span.m-listing__meta span.subtitle'
+
     request({
         url: `${base_url}/more?page=${page}`,
         headers: {'Content-type': 'application/json'},
         strictSSL: true,
         agentClass: Agent,
-        // agentOptions: {
-        //     socksHost: 'my-tor-proxy-host', // Defaults to 'localhost'.
-        //     socksPort: 9050, // Defaults to 1080.
-    
-        //     // Optional credentials
-        //     socksUsername: 'proxyuser',
-        //     socksPassword: 'p@ssw0rd',
-        // }
     }, function(err, res) {
         let data_arr = []
         if (!err) {
             let html = JSON.parse(res.body).html
             let $ = cheerio.load(html)     
-            let tag_a = 'li.m-listing a'  
-            $(tag_a).each((idx, ele)=>{
-
-                let tag_img = 'span.m-listing__img img' 
-                let tag_title = 'span.m-listing__meta strong.title'
-                let tag_author = 'span.m-listing__meta span.subtitle'
-
+            $(tag_base).each((idx, ele)=>{
                 let href = ele.attribs.href
                 let iiifid = $(tag_img, ele).attr('data-iiifid')
                 let desc = $(tag_img, ele).attr('alt')
@@ -44,13 +35,13 @@ let laod_more = (page, callback) => {
                     'title': title,
                     'author': author
                 })
-                console.log(`
-                href: ${href}, 
-                iiifid: ${iiifid}, 
-                desc: ${desc}, 
-                title: ${title}, 
-                author: ${author}
-                `)
+                // console.log(`
+                // href: ${href}, 
+                // iiifid: ${iiifid}, 
+                // desc: ${desc}, 
+                // title: ${title}, 
+                // author: ${author}
+                // `)
             })
             callback(data_arr)
         } else {
